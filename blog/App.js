663b1e07757d31cb1089231df2cedc4fe64db944
rj7shakya <shakyarajad1@gmail.com>
2020-05-17 21:53:44 +0545

    blog nav

---
+++ blog/App.js
@@ -0,0 +1,14 @@
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+import IndexScreen from './src/screens/IndexScreen';
+
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+  },
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs',
+    },
+  },
+);
