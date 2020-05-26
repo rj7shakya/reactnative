bfa4e595a3b631aa08dc2b6dafa9b8e5a2f7e5b6
rj7shakya <shakyarajad1@gmail.com>
2020-05-26 16:41:42 +0545

    basic app working

--- meals/navigation/MealsNavigation.js
+++ meals/navigation/MealsNavigation.js
@@ -1,9 +1,11 @@
 import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 import Colors from '../constants/Colors';
 
 const MealsNavigator = createStackNavigator(
@@ -29,4 +31,21 @@ const MealsNavigator = createStackNavigator(
   },
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: tabInfo => {},
+      },
+    },
+    Favorites: FavoritesScreen,
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  },
+);
+
+export default createAppContainer(MealsFavTabNavigator);
