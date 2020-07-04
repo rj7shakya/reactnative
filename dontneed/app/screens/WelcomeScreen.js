59e004245cd3eca069b2a0979ffaff9c6fa29a58
rj7shakya <shakyarajad1@gmail.com>
2020-07-04 09:39:29 +0545

    layouts managed

--- dontneed/app/screens/WelcomeScreen.js
+++ dontneed/app/screens/WelcomeScreen.js
@@ -5,35 +5,43 @@ import {
   View,
   ImageBackground,
   Dimensions,
+  Image,
 } from 'react-native';
 
 const WelcomeScreen = () => {
   return (
-    <View>
-      <ImageBackground
-        style={styles.background}
-        source={require('../assets/background.jpg')}>
-        <View style={styles.loginButton}></View>
-        <View style={styles.registerButton}></View>
-      </ImageBackground>
-      {/* <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View> */}
-    </View>
+    <ImageBackground
+      style={styles.background}
+      source={require('../assets/background.jpg')}>
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require('../assets/logo-red.png')} />
+        <Text>Sell What You Dont Need</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-    flexDirection: 'column',
     justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   loginButton: {
     width: '100%',
     height: 70,
     backgroundColor: '#fc5c65',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    alignItems: 'center',
   },
   registerButton: {
     width: '100%',
